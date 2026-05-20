79a2914b86d95aea97ffdb8b2ab7658c5c77a2b1
Mustafa  Abdelmgeed <mostafa012155@gmail.com>
2026-05-20 14:58:21 +0300

    Add media file settings and update URL configuration for serving media files

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 
@@ -17,3 +19,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_patterns)),
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
